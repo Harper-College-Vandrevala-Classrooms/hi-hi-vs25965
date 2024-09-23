7cbf09a64ebd80abeb977c974e1961f1ed605f46
vs25965 <vs25965@mail.harpercollege.edu>
2024-09-22 22:19:47 -0500

    New tests added

--- hi-hi project/hi-hi project.cpp
+++ hi-hi project/hi-hi project.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main()
 {
+	//test1
 	string QUA = "QUA";
 	string quw = "quw";
 	compare object (QUA, quw);
@@ -19,7 +20,7 @@ int main()
 	assert(object.strcmp_case_insensitive(QUA, quw) == -1);
 	cout << "Test succeeded!\nResult of case-insensitive comparison is: " << value << endl;
 
-
+	//test2
 	string Hi = "Hi";
 	string hi = "hi";
 	compare object2(Hi, hi);
@@ -29,7 +30,7 @@ int main()
 	assert(object2.strcmp_case_insensitive(Hi, hi) == 0);
 	cout << "Test succeeded!\nResult of case-insensitive comparison is: " << value2 << endl;
 
-
+	//test3
 	string Warah= "Warah";
 	string Asu = "Asu";
 	compare object3(Warah, Asu);
@@ -39,7 +40,26 @@ int main()
 	assert(object3.strcmp_case_insensitive(Warah, Asu) == 1);
 	cout << "Test succeeded!\nResult of case-insensitive comparison is: " << value3 << endl;
 	
+	//test4
+	string one = "string one";
+	string two = "string two";
+	compare object4(one, two);
+	int value4 = object4.strcmp_case_insensitive(one,two);
+
+	cout << "\nStarting tests for strings: " << one << "  and  " << two << endl;
+	assert(object4.strcmp_case_insensitive(one, two) == -1);
+	cout << "Test succeeded!\nResult of case-insensitive comparison is: " << value4 << endl;
 	
+	//test 5
+	string one_ = "CARAmelo";
+	string two_ = "Cartagena";
+	compare object5(one_, two_);
+	int value5 = object4.strcmp_case_insensitive(one_, two_);
+
+	cout << "\nStarting tests for strings: " << one_ << "  and  " << two_ << endl;
+	assert(object5.strcmp_case_insensitive(one_, two_) == 1);
+	cout << "Test succeeded!\nResult of case-insensitive comparison is: " << value5 << endl;
+
 
 	
 
